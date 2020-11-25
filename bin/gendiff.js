ee32ff354720087bc9949cc916b161d2e464fc26
Anastasia <anastasia.zhura@gmail.com>
2020-11-25 20:33:09 +0300

    update help output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import program from 'commander';
 
 program
   .version('1.0.0', '-V, --version', 'outpoot the version number')
-  .description('Compares two configuration files and show difference.');
-
+  .description('Compares two configuration files and show difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f --format <type>', 'output format');
 program.parse(process.argv);
